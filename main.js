7b79571458ecfab2c9458bddd43f8cf3b1ef8598
Shon Benayoun <shonba98@gmail.com>
2023-06-11 20:30:55 +0300

    Add comments to the code

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
+// Hard Coded variables
 const TASKS_KEY = "TaskList"
 let tasks = [];
 loadFromLocalStorage()
 
-
+// Add task Function
 function addTask() {
     event.preventDefault();
     const taskTextBox = document.getElementById("taskTextBox");
@@ -22,34 +23,39 @@ function addTask() {
     taskTextBox.focus();
 }
 
-function dateTemplate(date){
-    const [year , month, day] = date.split("-")
+// Change date display
+function dateTemplate(date) {
+    const [year, month, day] = date.split("-")
     return `${day}-${month}-${year}`;
 }
 
+// saving to local storage
 function saveToLocalStorage() {
     const str = JSON.stringify(tasks);
     localStorage.setItem(TASKS_KEY, str);
 }
 
+// loading and printing from local storage
 function loadFromLocalStorage() {
     const strTasks = localStorage.getItem(TASKS_KEY);
-    if(strTasks != null && strTasks.length > 0){
+    if (strTasks != null && strTasks.length > 0) {
         tasks = JSON.parse(strTasks);
     }
     displayTasksList()
 }
 
-function displayTasksList(){
+// print tasks
+function displayTasksList() {
     const tableRowContainer = document.getElementById("tableRowContainer");
     let html = "";
-    for (let i = 0; i <tasks.length; i++) {
+    for (let i = 0; i < tasks.length; i++) {
         html += `<td><span>${tasks[i].task}<br></span><span><div>${tasks[i].date}<br>${tasks[i].time}</div><div class="removeContainer"><a href="#" id="${i}" class="btn btn-danger removeButton" onclick="deleteTask(this)">X</a></div></span></td>`
     }
     tableRowContainer.innerHTML = html
 }
 
-function deleteTask(element){
+// remove task function
+function deleteTask(element) {
     const index = element.id;
     tasks.splice(index, 1);
     saveToLocalStorage();
